5ab744d45fb61cee761a2b5f841bbba32998d1ca
Maksimilijan Marosevic <m.marosevic@reply.de>
2024-11-23 15:48:29 +0100

    Improve push_front test

--- tests/test_push_front.c
+++ tests/test_push_front.c
@@ -13,30 +13,36 @@ int main() {
 	retval = int_vector_push_front(&ivec, 0);
 	assert(ivec.length == 1);
 	assert(ivec.capacity == 4);
+	assert(ivec.data[0] == 0);
 	assert(retval == 0);
 
 	retval = int_vector_push_front(&ivec, 1);
 	assert(ivec.length == 2);
 	assert(ivec.capacity == 4);
+	assert(ivec.data[0] == 1);
 	assert(retval == 0);
 
 	retval = int_vector_push_front(&ivec, 2);
 	assert(ivec.length == 3);
 	assert(ivec.capacity == 4);
+	assert(ivec.data[0] == 2);
 	assert(retval == 0);
 
 	retval = int_vector_push_front(&ivec, 3);
 	assert(ivec.length == 4);
 	assert(ivec.capacity == 4);
+	assert(ivec.data[0] == 3);
 	assert(retval == 0);
 
 	retval = int_vector_push_front(&ivec, 4);
 	assert(ivec.length == 5);
 	assert(ivec.capacity == 8);
+	assert(ivec.data[0] == 4);
 	assert(retval == 0);
 
 	retval = int_vector_push_front(&ivec, 5);
 	assert(ivec.length == 6);
 	assert(ivec.capacity == 8);
+	assert(ivec.data[0] == 5);
 	assert(retval == 0);
 }
